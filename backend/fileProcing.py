422a917935b9ab2b66b0f1aab7b1f5869608c2dd
林海 陈 <quartz010@outlook.com>
2018-06-05 17:37:39 +0800

    fixSS

--- backend/fileProcing.py
+++ backend/fileProcing.py
@@ -1,6 +1,15 @@
 # coding:utf-8
 import hashlib
 import json
+import time
+import requests
+
+
+import urllib
+import os
+import base64
+
+TIMEOUT = 3
 
 class FileProc(object):
     
@@ -24,11 +33,77 @@ class FileProc(object):
         self.file_info['hash'] = hs.hexdigest()
         self.file_info['more'] = [1,1,12,6,323,4]
 
-    def get_info(self):
+    def get_info(self, file=None):
+        self.file = file
         self.__name()
         self.__hash()
 
         print(json.dumps(self.file_info, sort_keys=True, indent=4, separators=(',', ': ')))
 
+
+
+class RemoteProc(object):
+    
+    def __init__(self, file = None):
+        self.r_url = {
+            'ocr':'https://api.ai.qq.com/fcgi-bin/ocr/ocr_generalocr'
+        }
+
+    def picOCR(self, file = None):
+        def encodePic():
+
+            with open('C:\\Users\\R4y\\Desktop\\a.png', 'rb') as f:
+                data = base64.b64encode(f.read())
+                return data
+
+        self.__httpReq(encodePic())
+
+    def __httpReq(self, image):
+  
+        def genSignString(parser):
+            uri_str = ''
+            for key in sorted(parser.keys()):
+                if key == 'app_key':
+                    continue
+                uri_str += "%s=%s&" % (key, urllib.parse.quote(str(parser[key]), safe = ''))
+            sign_str = uri_str + 'app_key=' + parser['app_key']
+
+            hash_md5 = hashlib.md5(sign_str.encode('utf8'))
+            return hash_md5.hexdigest().upper()
+
+        def req():
+            return requests.post( self.r_url['ocr'],data=keywords, json=None, timeout=TIMEOUT)
+
+        keywords = {
+            "app_id": "",
+            "time_stamp": "",
+            'nonce_str': '',
+            'sign': '',
+            'image': ''
+        }
+        time_stamp = int(time.time())
+        keywords['time_stamp'] = time_stamp
+        keywords['nonce_str'] = time_stamp
+        keywords['app_key'] = 'nZ6D4DlLUIkczG92'
+        keywords['app_id'] = '1106953556'
         
+        keywords['image'] = image
+
+        keywords['sign'] = genSignString(keywords)
+        try:
+            res  = req()
+            print(res.text)
+        except requests.exceptions.ConnectTimeout:
+            
+            print('[-] Connect Timeout!')
+            return False
+
+        
+
+        
+app_key = '1106953556'
+app_id = 'nZ6D4DlLUIkczG92'
+
+a = RemoteProc()
+a.picOCR()
 
--- backend/router.py
+++ backend/router.py
@@ -3,6 +3,7 @@ from fileProcing import FileProc
 from flask import Flask,render_template,request,redirect,url_for
 from werkzeug.utils import secure_filename
 import os
+import threading
 
 app = Flask(__name__)
 fproc = FileProc()
@@ -22,8 +23,9 @@ def upload():
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
         upload_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
-        fproc.set_file(f)
-        fproc.get_info()
+
+        t = threading.Thread(target=fproc.get_info,args=(f,))
+        t.start()
         
         return redirect(url_for('upload'))
 
