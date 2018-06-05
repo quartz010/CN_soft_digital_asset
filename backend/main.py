b1293804ebbf78c7d637769ef8a3d2bc5fe47a6f
林海 陈 <quartz010@outlook.com>
2018-06-05 13:21:10 +0800

    Basic Update

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+
+import hashlib
+from flask import Flask,render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello Flask!'
+    
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        upload_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        f.save(upload_path)
+        hs = hashlib.sha1()
+        hs.update(f.read())
+        print(hs.hexdigest())
+        return redirect(url_for('upload'))
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    # print(os.getcwd())
+    app.run(debug=True)
+
